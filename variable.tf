43051b1a4e5462fbc158988ebc5a1b4e1632858c
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-11-05 21:31:34 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,49 @@
+variable "region" {
+  type        = string
+  default     = ""
+  description = "enter region name"
+}
+
+
+
+variable "dynamo_name" {
+  type        = string
+  default     = ""
+  description = "enter dynamo db tabel name"
+}
+
+variable "billing_mode" {
+  type        = string
+  default     = ""
+  description = "enter billing_mode"
+}
+variable "dynamodb_table_tag" {
+  type        = string
+  default     = ""
+  description = "enter dynamodb_table_tag"
+}
+
+
+variable "projection_type" {
+  type        = string
+  default     = ""
+  description = "enter projection_type"
+}
+
+variable "environment_tag" {
+  type        = string
+  default     = ""
+  description = "enter projection_type"
+}
+
+variable "hash_key" {
+  type        = string
+  default     = ""
+  description = "enter hash_key"
+}
+
+variable "index_name_secondary_index" {
+  type        = string
+  default     = ""
+  description = "enter hash_key"
+}
