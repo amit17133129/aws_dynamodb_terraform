37f0fea78ff34e2744b85b89769516b48f644e40
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-11-05 21:02:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-# provider "aws" {
-#    region = "ap-south-1"
-# }
+provider "aws" {
+   region = "ap-south-1"
+}
+
+
+resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
+  name = var.aws_glue_catalog_database
+}
